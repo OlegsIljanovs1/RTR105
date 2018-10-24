f94af9155e2c79996c98aa3415689358fefed11e
OlegsIljanovs1 <42993070+OlegsIljanovs1@users.noreply.github.com>
2018-10-24 09:18:34 +0300

    Add files via upload

---
+++ dgr_20181024.py
@@ -0,0 +1,159 @@
+python
+Python 3.6.4 |Anaconda, Inc.| (default, Jan 16 2018, 18:10:19) 
+[GCC 7.2.0] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> zot = 'abc'
+>>> print(zot[5])
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+IndexError: string index out of range
+>>> cd RTR105
+  File "<stdin>", line 1
+    cd RTR105
+            ^
+SyntaxError: invalid syntax
+>>> s = 'Monty Python'
+>>> print(s[:2])
+Mo
+>>> print s([8:])
+  File "<stdin>", line 1
+    print s([8:])
+          ^
+SyntaxError: invalid syntax
+>>> print (s[:8])
+Monty Py
+>>> print (s[:])
+Monty Python
+>>> a = 'Hello
+  File "<stdin>", line 1
+    a = 'Hello
+             ^
+SyntaxError: EOL while scanning string literal
+>>> a= 'Hello
+  File "<stdin>", line 1
+    a= 'Hello
+            ^
+SyntaxError: EOL while scanning string literal
+>>> a = 'Hello'
+>>> b = a+
+  File "<stdin>", line 1
+    b = a+
+         ^
+SyntaxError: invalid syntax
+>>> b = a+'There'
+>>> print(b)
+HelloThere
+>>> c = a+'' + 'There'
+>>> print(c)
+HelloThere
+>>> c = a+' ' + 'There'
+>>> print(c)
+Hello There
+>>> history > history_20181024
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'history' is not defined
+>>> 
+KeyboardInterrupt
+>>> git clone https://github.com/OlegsIljanovs1/RTR105
+  File "<stdin>", line 1
+    git clone https://github.com/OlegsIljanovs1/RTR105
+            ^
+SyntaxError: invalid syntax
+>>> git clone https://github.com/login/RTR105
+  File "<stdin>", line 1
+    git clone https://github.com/login/RTR105
+            ^
+SyntaxError: invalid syntax
+>>> cd RTR105
+  File "<stdin>", line 1
+    cd RTR105
+            ^
+SyntaxError: invalid syntax
+>>> git clone
+  File "<stdin>", line 1
+    git clone
+            ^
+SyntaxError: invalid syntax
+>>> 
+KeyboardInterrupt
+>>> 
+>>> git clone https://github.com/OlegsIljanovs1/RTR105
+  File "<stdin>", line 1
+    git clone https://github.com/OlegsIljanovs1/RTR105
+            ^
+SyntaxError: invalid syntax
+>>> history > history_43242342
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'history' is not defined
+>>> 
+
+
+
+
+
+
+
+
+
+
+>>> fruit = 'banana'
+>>> letter = fruit[1]
+>>> print(letter)
+a
+>>> x = 3
+>>> w = fruit[x-1]
+>>> print(w)
+n
+>>> zot = 'abc'
+>>> print(zot[5])
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+IndexError: string index out of range
+>>> print(len(fruit))
+6
+>>> x = len(fruit)
+>>> print(x)
+6
+>>> word = 'banana'
+>>> count = 0
+>>> letter =='a'
+True
+>>> count = count + 1
+>>> print(count)
+1
+>>> print(letter)
+a
+>>> s = 'Monty Python'
+>>> print (s[0:4])
+Mont
+>>> print(s[6:7])
+P
+>>> print(s[6:20])
+Python
+>>> s = 'Monty Python'
+>>> print(s[:2])
+Mo
+>>> print(s[8:])
+thon
+>>> print(s[:])
+Monty Python
+>>> 
+KeyboardInterrupt
+>>> 
+KeyboardInterrupt
+>>> 
+KeyboardInterrupt
+>>> 
+KeyboardInterrupt
+>>> dgr_20181024.py
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'dgr_20181024' is not defined
+>>> cd /RTR105
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'cd' is not defined
+>>> exit()
+
