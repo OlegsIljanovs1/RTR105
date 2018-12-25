70f4013ae18fe82034acf3af9656a0955adc26af
OlegsIljanovs1 <42993070+OlegsIljanovs1@users.noreply.github.com>
2018-12-25 23:45:12 +0200

    Update 170.py

--- 170.py
+++ 170.py
@@ -4,25 +4,25 @@ from time import sleep
 def f(x):
     return sin(x)
 
-# Definejam argumenta x robezhas:
+# Definejam argumenta x robezas:
 a = 1.1
 b = 3.2
 
-# Aprekjinam funkcijas vertibas dotajos punktos:
+# Aprekinam funkcijas vertibas dotajos punktos:
 funa = f(a)
 funb = f(b)
 
-# paarbaudam, vai dotajaa intrvaalaa ir saknes:
+# parbaudam, vai dotaja intervaalaa ir saknes:
 if ( funa * funb > 0.0) :
     print("Dotajaa intervaalaa [%s, %s] saknju nav"%(a,b))
     sleep(1); exit()  # Zinjo uz ekraana, gaida 1 sec. un darbu pabeidz
 else:
     print("Dotajaa intervaalaa sakne(s) ir!")
 
-# Defineejam pricizitaati, ar kaadu meklesim sakni:
+# Definejam pricizitati, ar kaadu meklesim sakni:
 deltax = 0.01
 
-# Sashaurinam saknes mekleshanas robezhas:
+# Sashaurinam saknes meklesanas robezas:
 while (fabs(b-a) > deltax):
     x = (a+b)/2; funx = f(x)
     if ((funa*funx = f(x)):
