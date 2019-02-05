71fcdbba8209e41e65697b683838fd88e845a668
OlegsIljanovs1 <42993070+OlegsIljanovs1@users.noreply.github.com>
2019-02-05 09:52:03 +0200

    Add files via upload

---
+++ lab1_2dala.m
@@ -0,0 +1,10 @@
+% grafiku ieladēšana datorā
+% un nolasīšana ar peli
+A = imread('bilde1.png');
+B = imread('bilde2.png');
+figure(1),image(A)
+figure(2),image([500 800], [1 0],B)
+set(gca,'Ydir','normal')
+[x,y] = ginput(10)
+
+
